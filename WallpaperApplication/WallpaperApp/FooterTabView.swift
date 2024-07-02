d2d0f64b07c39d9dcccff78bf17199fe49eccb0b
spark-05 <>
2024-07-02 14:17:58 +0900

    no message

---
+++ WallpaperApplication/WallpaperApp/FooterTabView.swift
@@ -0,0 +1,65 @@
+//
+//  FooterTabView.swift
+//  WallpaperApp
+//
+//  Created by spark-05 on 2024/07/01.
+//
+
+import UIKit
+
+enum FooterTab{
+    case home
+}
+
+protocol FooterTabViewDelegate: AnyObject{
+    func footerTabView(_ footerTabView: FooterTabView, didselectTab: FooterTab)
+}
+
+class FooterTabView: UIView {
+    
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var contentView: UIView!
+
+    var delegate: FooterTabViewDelegate?
+
+    
+    @IBAction func didTapHome(_ sender: Any) {
+        delegate?.footerTabView(self, didselectTab: .home)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        load()
+        setup()
+    }
+    
+    required init?(coder aDecoder : NSCoder) {
+        super.init(coder: aDecoder)!
+        load()
+        setup()
+    }
+    
+    
+    //影を背景につけて丸みをつけるコード
+    func setup(){
+        func setup(){
+            shadowView.layer.shadowColor = UIColor.systemGray.cgColor
+            shadowView.layer.shadowOffset = CGSize(width: 0, height: 2)
+            shadowView.layer.shadowRadius = 4.0
+            shadowView.layer.shadowOpacity = 0.5
+            shadowView.layer.cornerRadius = 25.0
+            shadowView.layer.masksToBounds = false
+            shadowView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        }
+
+    }
+    
+    
+    func load(){
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView{
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+    }
+    
+}
